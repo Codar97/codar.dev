0eeaafc476e9ec4776099d29d61df1a77bb95c73
Codar <samuelc1997@hotmail.co.uk>
2022-03-17 12:07:01 +0000

    Downgrade to amcharts4 due to browser issues

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,8 +1,7 @@
 import React, {useState, useEffect} from 'react';
-import * as am5 from "@amcharts/amcharts5";
-import * as am5percent from "@amcharts/amcharts5/percent";
-import am5themes_Animated from "@amcharts/amcharts5/themes/Animated";
-import am5themes_Dark from "@amcharts/amcharts5/themes/Dark";
+import * as am4core from "@amcharts/amcharts4/core";
+import * as am4charts from "@amcharts/amcharts4/charts";
+import am4themes_animated from "@amcharts/amcharts4/themes/animated";
 import colors from '../projects/colors.json';
 
 function Home() {
@@ -44,33 +43,27 @@ function Experience() {
             title: "Ivanti",
             subTitle: "(Previously Pulse Secure)",
             role: "Software Engineer",
-            description: ['Developed a UI component library  using React to be used across major products in the company.',
-                'Developed and integrated product licence management and gateway management tools for the product line (ZTA,PCS,vTM).',
-                'Developed and maintained a Web Application Firewall (vWAF).'],
+            description: ['Developed a UI component library  using React to be used across major products in the company.', 'Developed and integrated product licence management and gateway management tools for the product line (ZTA,PCS,vTM).', 'Developed and maintained a Web Application Firewall (vWAF).'],
             tools: ["JavaScript", "Python", "React", "Jenkins", "Docker", "Kubernetes", "Agile"],
             date: "2018-present",
             location: "Cambridge"
-        },
-        "freelance": {
+        }, "freelance": {
             title: "Freelance",
             role: "Freelancer",
-            description: ['Contributed to the development of open source projects',
-                'Developed a MMO game in Java with a small player base of ~200',
-                'Created lots of random fun projects'],
+            description: ['Contributed to the development of open source projects', 'Developed a MMO game in Java with a small player base of ~200', 'Created lots of random fun projects'],
             tools: ["Python", "Java", "JavaScript", "React", "Rust", "C#", "C++"],
         }
     };
     const experienceList = Object.keys(experience);
     const ExperienceCard = ({title, subTitle, role, description, date, location, tools}) => {
         return (<div className="experience__card">
-                <div className="experience__card__role">{role}</div>
-                <div className="experience__card__sub-title">
-                    {title} {subTitle} {location ? `- ${location}` : ""} {date ? `- ${date}` : ""}
-                </div>
-                <ul className="experience__card__description">{description.map(item => <li>{item}</li>)}</ul>
-                <div className="experience__card__tools">{tools.join(", ")}</div>
+            <div className="experience__card__role">{role}</div>
+            <div className="experience__card__sub-title">
+                {title} {subTitle} {location ? `- ${location}` : ""} {date ? `- ${date}` : ""}
             </div>
-        );
+            <ul className="experience__card__description">{description.map(item => <li>{item}</li>)}</ul>
+            <div className="experience__card__tools">{tools.join(", ")}</div>
+        </div>);
     }
     return (<div className="experience">
         <div className="experience__navigation">
@@ -92,38 +85,29 @@ function TechnicalSkills() {
     const technicalSkills = {
         'JavaScript': {
             skills: ['Proficient using frameworks including React.', 'Industry experience.', 'TypeScript experience.', 'Experience using Node.', 'Mobile App development using React Native.']
-        },
-        'Python': {
+        }, 'Python': {
             skills: ['Proficient using testing frameworks including selenium.', 'Industry experience.', 'Test driven development.', 'Webserver development experience.']
-        },
-        'Java': {
+        }, 'Java': {
             skills: ['Strong Object Orientated Programming skills.', 'Application development.', 'GUI Developmen.t', 'Experience using Maven.', 'Webserver development.']
-        },
-        'Rust': {
+        }, 'Rust': {
             skills: ['Experience Object Oriented Programming in Rust.', 'Experience with functional programming in Rust.']
-        },
-        'Haskell': {
+        }, 'Haskell': {
             skills: ['Strong functional programming skills.']
-        },
-        'Erlang': {
+        }, 'Erlang': {
             skills: ['Strong functional programming skills.', 'Network protocol development.']
-        },
-        'SQL': {
+        }, 'SQL': {
             skills: ['Experience using SQL databases.', 'Experience using SQL variations such as MYSQL & PostgreSQL.']
-        },
-        'PHP': {
+        }, 'PHP': {
             skills: ['Experience using PHP frameworks including CodeIgniter.']
-        },
-        'C++': {
+        }, 'C++': {
             skills: ['Embedded system development.', 'Object Oriented Programming skills in C++.', 'GUI program development.', 'Game engine development using Unity and Unreal Engine.']
-        },
-        'C#': {
+        }, 'C#': {
             skills: ['GUI program development.', 'Game engine development using Unity and Unreal Engine.']
         },
     }
     return (<div className="technical-skills">
         <Chart className={sideVisible ? "open" : ""} setSelected={setSelected} setSideVisible={(open) => {
-            setSideVisible(open === undefined ? true : !open)
+            setSideVisible(open === undefined ? true : open)
         }}/>
         <div className={`technical-skills__side-panel${sideVisible ? " technical-skills__side-panel--open" : ""}`}>
             <div className="technical-skills__side-panel__title">
@@ -140,70 +124,47 @@ function TechnicalSkills() {
 
 function Chart({setSelected, setSideVisible, className}) {
     useEffect(() => {
-        let root = am5.Root.new("chartdiv");
-        let chart = root.container.children.push(am5percent.PieChart.new(root, {
-            layout: root.verticalLayout,
-            innerRadius: am5.percent(50)
-        }));
-        root.setThemes([
-            am5themes_Dark.new(root),
-            am5themes_Animated.new(root)
+        am4core.useTheme(am4themes_animated);
+        let chart = am4core.create("chartdiv", am4charts.PieChart);
+        let pieSeries = chart.series.push(new am4charts.PieSeries());
+        pieSeries.dataFields.value = "value";
+        pieSeries.dataFields.category = "category";
+        chart.innerRadius = am4core.percent(50);
+        pieSeries.slices.template.cursorOverStyle = [{
+            "property": "cursor", "value": "pointer"
+        }];
+        pieSeries.alignLabels = false;
+        pieSeries.slices.template.propertyFields.fill = "color";
+        pieSeries.ticks.template.disabled = true;
 
-        ]);
-        let series = chart.series.push(am5percent.PieSeries.new(root, {
-            valueField: "value",
-            categoryField: "category",
-            tooltip: am5.Tooltip.new(root, {
-                labelText: "{category}"
-            }),
-            alignLabels: false
-        }));
-        series.slices.template.events.on("click", function (ev) {
-            setSelected(ev.target._dataItem.dataContext.category)
-            setSideVisible(ev.target.get("active"))
-            series.slices.each(function (slice) {
-                if (slice !== ev.target && slice.get("active")) {
-                    slice.set("active", false);
+        pieSeries.labels.template.fontFamily = ['source-code-pro', 'Menlo', 'Monaco', 'Consolas', 'Courier New'];
+        pieSeries.labels.template.fill = "#9ba2b2";
+        pieSeries.labels.template.text = "{category}";
+        pieSeries.slices.template.tooltipText = "{category}"
+
+        pieSeries.slices.template.events.on("hit", function (ev) {
+            let series = ev.target.dataItem.component;
+            setSelected(ev.target.dataItem.properties.category)
+            setSideVisible(ev.target.dataItem._slice.isActive)
+            series.slices.each(function (item) {
+                if (item.isActive && item != ev.target) {
+                    item.isActive = false;
                 }
             })
         });
-        series.labels.template.setAll({
-            fill: am5.color(0x9ba2b2),
-            fontSize: '14px',
-            text: "{category}",
-            fontFamily: ['source-code-pro', 'Menlo', 'Monaco', 'Consolas', 'Courier New'],
-            centerX: 0,
-            centerY: 0
-        });
-        series.get("colors").set("colors", [
-            am5.color(colors['JavaScript'].color),
-            am5.color(colors['Python'].color),
-            am5.color(colors['Java'].color),
-            am5.color(colors['C++'].color),
-            am5.color(colors['C#'].color),
-            am5.color(colors['Rust'].color),
-            am5.color(colors['Haskell'].color),
-            am5.color(colors['Erlang'].color),
-            am5.color(colors['PHP'].color),
-            am5.color(colors['SQL'].color),
+        chart.data = [
+            {value: 10, category: "JavaScript", color: colors["JavaScript"].color},
+            {value: 8, category: "Python", color: colors["Python"].color},
+            {value: 9, category: "Java", color: colors["Java"].color},
+            {value: 8, category: "C++", color: colors["C++"].color},
+            {value: 8, category: "C#", color: colors["C#"].color},
+            {value: 7, category: "Rust", color: colors["Rust"].color},
+            {value: 6, category: "Haskell", color: colors["Haskell"].color},
+            {value: 6, category: "Erlang", color: colors["Erlang"].color},
+            {value: 5, category: "PHP", color: colors["PHP"].color},
+            {value: 5, category: "SQL", color: colors["SQL"].color}];
 
-        ]);
-        series.data.setAll([
-            {value: 10, category: "JavaScript"},
-            {value: 8, category: "Python"},
-            {value: 9, category: "Java"},
-            {value: 8, category: "C++"},
-            {value: 8, category: "C#"},
-            {value: 7, category: "Rust"},
-            {value: 6, category: "Haskell"},
-            {value: 6, category: "Erlang"},
-            {value: 5, category: "PHP"},
-            {value: 5, category: "SQL"},
-        ]);
-
-        series.appear(1000, 100);
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
+    }, [])
     return (<div className={className} id={'chartdiv'}/>)
 }
 
